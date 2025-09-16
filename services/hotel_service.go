18e07ade602e6e0403ee4723ad8506675a9de03c
Cao Duy Lâm <duylamasd@gmail.com>
2025-09-17 03:06:23 +0700

    fix(service): return empty array if no hotels found

--- services/hotel_service.go
+++ services/hotel_service.go
@@ -26,16 +26,43 @@ func (s *hotelService) FindByHotelID(ctx context.Context, hotelID string) (*sqlc
 }
 
 func (s *hotelService) FindByDestinationID(ctx context.Context, destinationID string) ([]*sqlc.Hotel, error) {
-	return s.db.Queries.FindHotelsByDestinationID(ctx, destinationID)
+	hotels, err := s.db.Queries.FindHotelsByDestinationID(ctx, destinationID)
+	if err != nil {
+		return nil, err
+	}
+
+	if hotels == nil {
+		return []*sqlc.Hotel{}, nil
+	}
+
+	return hotels, nil
 }
 
 func (s *hotelService) FindByHotelIDs(ctx context.Context, hotelIDs []string) ([]*sqlc.Hotel, error) {
-	return s.db.Queries.FindHotelsByHotelIDs(ctx, hotelIDs)
+	hotels, err := s.db.Queries.FindHotelsByHotelIDs(ctx, hotelIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	if hotels == nil {
+		return []*sqlc.Hotel{}, nil
+	}
+
+	return hotels, nil
 }
 
 func (s *hotelService) FindByDestinationAndHotelIDs(ctx context.Context, destinationID string, hotelIDs []string) ([]*sqlc.Hotel, error) {
-	return s.db.Queries.FindHotelsByDestinationAndHotelIDs(ctx, sqlc.FindHotelsByDestinationAndHotelIDsParams{
+	hotels, err := s.db.Queries.FindHotelsByDestinationAndHotelIDs(ctx, sqlc.FindHotelsByDestinationAndHotelIDsParams{
 		DestinationID: destinationID,
 		HotelIds:      hotelIDs,
 	})
+	if err != nil {
+		return nil, err
+	}
+
+	if hotels == nil {
+		return []*sqlc.Hotel{}, nil
+	}
+
+	return hotels, nil
 }
